0f47fa9fa1557e7e6abd3a8b4cb8c2b7f87ceb7f
avinash dsilva <evnix.com@gmail.com>
2016-07-18 20:33:35 +0100

    gofmt'ed the code.

--- boltdbweb.go
+++ boltdbweb.go
@@ -3,10 +3,12 @@ package main
 import "github.com/gin-gonic/gin"
 import "github.com/evnix/boltdbweb/web"
 
-import ("fmt"
-		log "github.com/Sirupsen/logrus"
-		 "github.com/boltdb/bolt"
-		 "os")		
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/boltdb/bolt"
+	"os"
+)
 
 var db *bolt.DB
 
@@ -15,51 +17,48 @@ func main() {
 	fmt.Print(" ")
 	log.Info("starting boltdb-browser..")
 
-	if(len(os.Args)<2){
+	if len(os.Args) < 2 {
 
-		fmt.Println("Usage: "+os.Args[0]+" <DBfilename>[required] <port>[optional]")
+		fmt.Println("Usage: " + os.Args[0] + " <DBfilename>[required] <port>[optional]")
 		os.Exit(0)
 	}
 
 	var err error
-	db,err=bolt.Open(os.Args[1], 0600, nil)
-	boltbrowserweb.Db=db
-	
-	if(err!=nil){
+	db, err = bolt.Open(os.Args[1], 0600, nil)
+	boltbrowserweb.Db = db
+
+	if err != nil {
 
 		fmt.Println(err)
 		os.Exit(0)
 	}
- 
 
 	r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-    r.GET("/", boltbrowserweb.Index)
+	r.GET("/", boltbrowserweb.Index)
 
-    r.GET("/buckets", boltbrowserweb.Buckets)
-    r.POST("/createBucket", boltbrowserweb.CreateBucket)
-    r.POST("/put", boltbrowserweb.Put)
-    r.POST("/get", boltbrowserweb.Get)
-    r.POST("/deleteKey", boltbrowserweb.DeleteKey)
-    r.POST("/deleteBucket", boltbrowserweb.DeleteBucket)
-    r.POST("/prefixScan", boltbrowserweb.PrefixScan)
+	r.GET("/buckets", boltbrowserweb.Buckets)
+	r.POST("/createBucket", boltbrowserweb.CreateBucket)
+	r.POST("/put", boltbrowserweb.Put)
+	r.POST("/get", boltbrowserweb.Get)
+	r.POST("/deleteKey", boltbrowserweb.DeleteKey)
+	r.POST("/deleteBucket", boltbrowserweb.DeleteBucket)
+	r.POST("/prefixScan", boltbrowserweb.PrefixScan)
 
+	r.Static("/web", "./web")
 
-    r.Static("/web", "./web")
+	port := "8080"
 
-    port:="8080";
+	if len(os.Args) > 2 {
 
-    if(len(os.Args)>2){
+		port = os.Args[2]
+	}
 
-    	port=os.Args[2]
-    }
-
-    r.Run(":"+port)
-
+	r.Run(":" + port)
 
 }
--- web/index.go
+++ web/index.go
@@ -1,290 +1,255 @@
 package boltbrowserweb
 
-
-import ("github.com/gin-gonic/gin"
-"github.com/boltdb/bolt"
-"fmt"
-"bytes")
-
-
+import (
+	"bytes"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/gin-gonic/gin"
+)
 
 var Db *bolt.DB
 
 func Index(c *gin.Context) {
 
-		c.Redirect(301, "/web/html/layout.html")
-	
-}
+	c.Redirect(301, "/web/html/layout.html")
 
+}
 
 func CreateBucket(c *gin.Context) {
 
-
-
-if(c.PostForm("bucket")==""){
-	 c.String(200, "no bucket name | n");
-}
+	if c.PostForm("bucket") == "" {
+		c.String(200, "no bucket name | n")
+	}
 
 	Db.Update(func(tx *bolt.Tx) error {
-    	b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
-    	b=b
-    	if err != nil {
-        	return fmt.Errorf("create bucket: %s", err)
-   		 }
-    return nil
+		b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
+		b = b
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
 	})
-		c.String(200, "ok");
-	
+	c.String(200, "ok")
+
 }
-
-
 
 func DeleteBucket(c *gin.Context) {
 
-
-	if(c.PostForm("bucket")==""){
-		 c.String(200, "no bucket name | n");
+	if c.PostForm("bucket") == "" {
+		c.String(200, "no bucket name | n")
 	}
 
 	Db.Update(func(tx *bolt.Tx) error {
-    	err := tx.DeleteBucket([]byte(c.PostForm("bucket")))
-    	
-    	if err != nil {
+		err := tx.DeleteBucket([]byte(c.PostForm("bucket")))
 
-    		c.String(200, "error no such bucket | n");
-        	return fmt.Errorf("bucket: %s", err)
-   		 }
+		if err != nil {
 
+			c.String(200, "error no such bucket | n")
+			return fmt.Errorf("bucket: %s", err)
+		}
 
-    return nil
+		return nil
 	})
-	
-	c.String(200, "ok");
-	
-}
 
+	c.String(200, "ok")
+
+}
 
 func DeleteKey(c *gin.Context) {
 
-
-	if(c.PostForm("bucket")=="" || c.PostForm("key")==""){
-		 c.String(200, "no bucket name or key | n");
+	if c.PostForm("bucket") == "" || c.PostForm("key") == "" {
+		c.String(200, "no bucket name or key | n")
 	}
 
 	Db.Update(func(tx *bolt.Tx) error {
-    	b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
-    	b=b
-    	if err != nil {
+		b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
+		b = b
+		if err != nil {
 
-    		c.String(200, "error no such bucket | n");
-        	return fmt.Errorf("bucket: %s", err)
-   		 }
+			c.String(200, "error no such bucket | n")
+			return fmt.Errorf("bucket: %s", err)
+		}
 
-   		 err = b.Delete([]byte(c.PostForm("key")))
+		err = b.Delete([]byte(c.PostForm("key")))
 
-   		 if err != nil {
+		if err != nil {
 
-    		c.String(200, "error Deleting KV | n");
-        	return fmt.Errorf("delete kv: %s", err)
-   		 }
+			c.String(200, "error Deleting KV | n")
+			return fmt.Errorf("delete kv: %s", err)
+		}
 
-    return nil
+		return nil
 	})
-	
-	c.String(200, "ok");
-	
+
+	c.String(200, "ok")
+
 }
 
 func Put(c *gin.Context) {
 
-
-	if(c.PostForm("bucket")=="" || c.PostForm("key")==""){
-		 c.String(200, "no bucket name or key | n");
+	if c.PostForm("bucket") == "" || c.PostForm("key") == "" {
+		c.String(200, "no bucket name or key | n")
 	}
 
 	Db.Update(func(tx *bolt.Tx) error {
-    	b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
-    	b=b
-    	if err != nil {
+		b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
+		b = b
+		if err != nil {
 
-    		c.String(200, "error  creating bucket | n");
-        	return fmt.Errorf("create bucket: %s", err)
-   		 }
+			c.String(200, "error  creating bucket | n")
+			return fmt.Errorf("create bucket: %s", err)
+		}
 
-   		 err = b.Put([]byte(c.PostForm("key")), []byte(c.PostForm("value")))
+		err = b.Put([]byte(c.PostForm("key")), []byte(c.PostForm("value")))
 
-   		 if err != nil {
+		if err != nil {
 
-    		c.String(200, "error writing KV | n");
-        	return fmt.Errorf("create kv: %s", err)
-   		 }
+			c.String(200, "error writing KV | n")
+			return fmt.Errorf("create kv: %s", err)
+		}
 
-    return nil
+		return nil
 	})
-	
-	c.String(200, "ok");
-	
-}
 
+	c.String(200, "ok")
+
+}
 
 func Get(c *gin.Context) {
 
-	res:= []string{"nok",""}
+	res := []string{"nok", ""}
 
-	if(c.PostForm("bucket")=="" || c.PostForm("key")==""){
+	if c.PostForm("bucket") == "" || c.PostForm("key") == "" {
 
-		res[1]="no bucket name or key | n"
-		 c.JSON(200, res);
+		res[1] = "no bucket name or key | n"
+		c.JSON(200, res)
 	}
 
+	Db.View(func(tx *bolt.Tx) error {
 
-		Db.View(func(tx *bolt.Tx) error {
-		
-			
-		    b := tx.Bucket([]byte(c.PostForm("bucket")))
+		b := tx.Bucket([]byte(c.PostForm("bucket")))
 
-		    if(b!=nil){
+		if b != nil {
 
-		    	v := b.Get([]byte(c.PostForm("key")))
+			v := b.Get([]byte(c.PostForm("key")))
 
-		    	res[0]="ok"
-		    	res[1]= string(v)
+			res[0] = "ok"
+			res[1] = string(v)
 
-		    	fmt.Printf("Key: %s\n", v)
+			fmt.Printf("Key: %s\n", v)
 
-			}else{
+		} else {
 
-				res[1]="error opening bucket| does it exist? | n"
-				
-			}
-		    return nil
+			res[1] = "error opening bucket| does it exist? | n"
 
+		}
+		return nil
 
-        })
+	})
 
+	c.JSON(200, res)
 
-         c.JSON(200, res);
-
-		
-	
 }
 
 type Result struct {
-    Result string
-    M map[string]string
+	Result string
+	M      map[string]string
 }
 
 func PrefixScan(c *gin.Context) {
 
-	res:= Result{Result:"nok"}
+	res := Result{Result: "nok"}
 
-	res.M= make(map[string]string)
+	res.M = make(map[string]string)
 
-	
-	if(c.PostForm("bucket")=="" ){
+	if c.PostForm("bucket") == "" {
 
-		res.Result="no bucket name | n"
-		 c.JSON(200, res);
+		res.Result = "no bucket name | n"
+		c.JSON(200, res)
 	}
 
-	count:=0
+	count := 0
 
-	if(c.PostForm("key")==""){
+	if c.PostForm("key") == "" {
 
 		Db.View(func(tx *bolt.Tx) error {
-		    // Assume bucket exists and has keys
-		    b := tx.Bucket([]byte(c.PostForm("bucket")))
+			// Assume bucket exists and has keys
+			b := tx.Bucket([]byte(c.PostForm("bucket")))
 
+			if b != nil {
 
-		    if(b!=nil){
+				c := b.Cursor()
 
-		    c := b.Cursor()
+				for k, v := c.First(); k != nil; k, v = c.Next() {
+					res.M[string(k)] = string(v)
 
-		    
+					if count > 2000 {
+						break
+					}
+					count++
+				}
 
-		    for k, v := c.First(); k != nil; k, v = c.Next() {
-		       res.M[string(k)]=string(v)
+				res.Result = "ok"
 
-		       if(count>2000){
-		       		break;
-		   		}
-		   		count++
-		    }
+			} else {
 
-		     res.Result="ok"
-
-		    }else{
-
-				res.Result="no such bucket available | n"
+				res.Result = "no such bucket available | n"
 
 			}
 
-		    return nil
+			return nil
 		})
 
+	} else {
 
-	}else{
+		Db.View(func(tx *bolt.Tx) error {
+			// Assume bucket exists and has keys
+			b := tx.Bucket([]byte(c.PostForm("bucket"))).Cursor()
 
-        Db.View(func(tx *bolt.Tx) error {
-  		 // Assume bucket exists and has keys
-		    b := tx.Bucket([]byte(c.PostForm("bucket"))).Cursor()
+			if b != nil {
 
-		    if(b!=nil){
+				prefix := []byte(c.PostForm("key"))
 
-			    prefix := []byte(c.PostForm("key"))
+				for k, v := b.Seek(prefix); bytes.HasPrefix(k, prefix); k, v = b.Next() {
+					res.M[string(k)] = string(v)
+					if count > 2000 {
+						break
+					}
+					count++
+				}
 
+				res.Result = "ok"
 
-			    for k, v := b.Seek(prefix); bytes.HasPrefix(k, prefix); k, v = b.Next() {
-			        res.M[string(k)]=string(v)
-			        if(count>2000){
-		       			break;
-		   			}
-		   			count++
-			    }
+			} else {
 
-			    res.Result="ok"
-
-			}else{
-
-				res.Result="no such bucket available | n"
+				res.Result = "no such bucket available | n"
 
 			}
 
-		    return nil
+			return nil
 		})
 
+	}
 
- 	}
+	c.JSON(200, res)
 
-
-         c.JSON(200, res);
-
-		
-	
 }
-
-
 
 func Buckets(c *gin.Context) {
 
-	
+	res := []string{}
 
-	res:= []string{}
+	Db.View(func(tx *bolt.Tx) error {
 
-		Db.View(func(tx *bolt.Tx) error {
-		
-			
-        return tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
-            
-            b:=[]string{string(name)}
-            res=append(res, b...) 
-            return nil
-       	 })
+		return tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
 
+			b := []string{string(name)}
+			res = append(res, b...)
+			return nil
+		})
 
+	})
 
-        })
+	c.JSON(200, res)
 
-		c.JSON(200, res)
-	
 }
